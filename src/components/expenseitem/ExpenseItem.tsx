5a5671e8a433184a7f90a9b1f2ab4d785e3df1fe
unknown <lirimmuqolli@gmail.com>
2023-09-27 17:19:53 +0200

    update changed style for expenseItem

--- src/components/expenseitem/ExpenseItem.tsx
+++ src/components/expenseitem/ExpenseItem.tsx
@@ -14,14 +14,6 @@ interface ExpenseData {
 }
 
 const ExpenseItem: React.FC<ExpenseData> = ({ expenseData }) => {
-  const formatDate = (date: Date) => {
-    const options: Intl.DateTimeFormatOptions = {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    };
-    return date.toLocaleDateString(undefined, options);
-  };
 
   return (
     <div className='expense-item-container'>
